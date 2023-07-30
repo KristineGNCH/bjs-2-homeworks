63098bdbc46ddb93b9aa7aba71b22695fe617354
Kristine Goncharenko <kristine.gnch@gmail.com>
2023-07-30 19:59:31 +0300

    Выполнено задание 3

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -93,4 +93,24 @@ console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9)); // [2, 4, 6, 
 console.log(averageEvenElementsWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35)); // [64, 10, 40] => 38
 
 function makeWork(arrOfArr, func) {
+
+
+  let maxWorkerResult = -Infinity;
+
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const Arr = func(...arrOfArr[i]);
+
+    if (Arr > maxWorkerResult) {
+      maxWorkerResult = Arr;
+    }
+  }
+
+  return maxWorkerResult;
 }
+
+const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]];
+console.log(makeWork(arr, summElementsWorker)); // максимум из 61, 206, 328, 284 => 328
+console.log(makeWork(arr, differenceMaxMinWorker)); // максимум из 10, 86, 44, 66 => 86
+console.log(makeWork(arr, differenceEvenOddWorker)); // максимум из 39, -6, -184, 92 => 92
+console.log(makeWork(arr, averageEvenElementsWorker)); // максимум из 12.5, 33.333, 72, 62.666 => 72
+
